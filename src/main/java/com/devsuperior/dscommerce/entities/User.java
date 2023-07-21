51633597793b1418c661e71efd0a1e5746a4f1d2
Thiago Fernandes Canonici <t_canonici@hotmail.com>
2023-07-21 20:46:06 -0300

    Order

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +23,9 @@ public class User {
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -81,6 +87,9 @@ public class User {
 		this.password = password;
 	}
 
-	
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	
 }
