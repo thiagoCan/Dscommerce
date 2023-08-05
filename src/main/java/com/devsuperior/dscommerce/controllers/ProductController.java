b4c43f349b13e01d28171ad322a57b616c4b814a
Thiago Fernandes Canonici <t_canonici@hotmail.com>
2023-08-04 22:29:02 -0300

    GET product by id - com tres camadas -

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,14 +1,13 @@
 package com.devsuperior.dscommerce.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscommerce.entities.Product;
-import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
 
 @RestController
 //Configurando a rota
@@ -16,13 +15,12 @@ import com.devsuperior.dscommerce.repositories.ProductRepository;
 public class ProductController {
 
 	@Autowired
-	private ProductRepository repository;
+	private ProductService service;
 		
-	@GetMapping
-	public String teste() {
-		Optional<Product> result = repository.findById(1L);
-		Product product = result.get();
-		return product.getName();
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(id);
+		return dto;
 	}
 	
 }
